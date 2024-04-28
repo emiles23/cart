e72d8342df9e2d7b885fb4bd06a39382b2b40eb9
emiles23 <emilesalbornoz@gmail.com>
2024-04-28 17:55:50 -0400

    add pinia

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createPinia } from 'pinia'
 import './tailwind.css'
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+app.mount('#app')
+
+
