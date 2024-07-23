f650a30cf0669ff8969641f5b4239bea331b36ee
emiles23 <emilesalbornoz@gmail.com>
2024-07-23 18:23:33 -0400

    refactor

--- src/App.vue
+++ src/App.vue
@@ -27,9 +27,15 @@
 import ProductCard from "./components/ProductCard.vue";
 import Main from "./layout/Main.vue";
 import { shoppingCartStore } from "./store.js";
-import { definitions } from "./store.js";
-import { useSearchEngineStore } from "./store/searchEngine.js"
+
+// import { definitions } from "./store.js";
+
+// pinia store
 import { mapState } from 'pinia'
+import { useSearchEngineStore } from "./store/searchEngine.js"
+import { useDefinitionsStore } from "./store/definitions.js"
+// import { useShoppingCartStoreStore } from "./store/shoppingCartStore.js"
+
 
 export default {
   components: {
@@ -42,105 +48,9 @@ export default {
       showCart: false,
       showDiscount: false,
       showCurrentiIndex: null,
-      definitions,
       shoppingCartStore,
+      // definitions,
 
-
-      products: [
-        {
-          name: 'Camisa sin manga',
-          brand: 'Shein',
-          price: 12,
-          img: "img/store/img1.png"
-        },
-        {
-          name: 'Blazer unicolor',
-          brand: 'Shein',
-          price: 15,
-          img: "img/store/img2.png"
-        },
-        {
-          name: 'Pantalones jogger',
-          brand: 'Shein',
-          price: 18,
-          img: "/img/store/img3.png"
-        },
-        {
-          name: '55 pulgadas Smart tv 4k',
-          brand: 'Samsung', price: 799,
-          img: "img/store/img4.png"
-        },
-        {
-          name: '32 pulgadas SFire TV Omni Series ',
-          brand: 'Samsung',
-          price: 325,
-          img: "img/store/img5.png"
-        },
-        {
-          name: '75 pulgadas, clase Crystal UHD, serie AU8000, 4K, UHD, HDR, Smart TV',
-          brand: 'Samsung',
-          price: 500,
-          img: "img/store/img6.png"
-        },
-
-        {
-          name: '(AMD Ryzen 5 5600X, RTX 3060, 16GB 3600Mhz',
-          brand: 'Dell',
-          price: 300,
-          img: "img/store/img7.png"
-        },
-        {
-          name: 'Computadora OptiPlex personalizada de escritorio Intel Core i5-6500',
-          brand: 'Dell',
-          price: 23,
-          img: "img/store/img8.png"
-        },
-        {
-          name: 'procesador Intel Core i7-11700F, GeForce RTX 3060, 32 GB de RAM, 1 TB ',
-          brand: 'Dell',
-          price: 600,
-          img: "img/store/img9.png"
-        },
-
-        {
-          name: 'Zapatos deportivos de correr para hombre',
-          brand: 'Adidas',
-          price: 300,
-          img: "img/store/img10.png"
-        },
-        {
-          name: 'Tenis para correr para mujer',
-          brand: 'Adidas',
-          price: 150,
-          img: "img/store/img11.png"
-        },
-        {
-          name: 'Zapatos planos Belice estilo ballet para mujer',
-          brand: 'Adidas',
-          price: 185,
-          img: "img/store/img12.png"
-        },
-
-        {
-          name: 'Plancha De Cabello Professional 450°F, plancha de pelo de cerámica ',
-          brand: 'Rucha',
-          price: 260,
-          img: "img/store/img13.png"
-        },
-        {
-          name: 'Conair Plancha plana de cerámica doble, 1 pulgada',
-          brand: 'Rucha',
-          price: 20,
-          img: "img/store/img14.png"
-        },
-        {
-          name: 'Titanium Ionic Hair Straightener, Professional Flat Iron For All Hair+ Types',
-          brand: 'Rucha',
-          price: 10,
-          img: "img/store/img15.png"
-        },
-
-      ],
     }
   },
   updated() {
@@ -151,14 +61,12 @@ export default {
 
   methods: {
 
-
     // changeDarkMode() {
     //   document.documentElement.classList.toggle("dark")
     // },
 
-
     getDiscountProductObject(product) {
-      return definitions.discounts.find(discount => discount.brand == product.brand)?.products
+      return this.discounts.find(discount => discount.brand == product.brand)?.products
         ?.find(productDiscount => productDiscount.name == product.name)
     },
     getProductDiscount(product) {
@@ -198,8 +106,11 @@ export default {
   },
 
   computed: {
-
+    
     ...mapState(useSearchEngineStore, ['search']),
+    ...mapState(useDefinitionsStore, ['discounts', 'discountGroups', 'products']),
+    // ...mapState(useShoppingCartStoreStore, ['products']),
+
     productsWithDicount() {
       return this.products.filter(product => product.name.toLowerCase().includes(this.search.toLowerCase())).map(product => {
         product.discountedPrice = this.getDiscountedPrice(product)
