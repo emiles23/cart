8f11b1c0a2934e5689135889c8dfdfd1385a725c
emiles23 <emilesalbornoz@gmail.com>
2024-07-25 22:48:46 -0400

    add

---
+++ src/pages/Checkout.vue
@@ -0,0 +1,97 @@
+
+<template>
+  <form>
+    <div class="space-y-12">
+
+      <div class="grid grid-cols-2 gap-x-20">
+        <div>
+          <div class="border-b border-gray-900/10 pb-12">
+            <h2 class="text-xl font-semibold leading-7 text-gray-900">Información de Contacto</h2>
+            <div class="sm:col-span-4">
+              <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email
+                address</label>
+              <div class="mt-2">
+                <input id="email" name="email" type="email" autocomplete="email" class="
+                  block w-full 
+                  rounded-md border-0 
+                  py-1.5 text-gray-900 
+                  shadow-sm ring-1 ring-inset
+                   ring-gray-300 
+                   placeholder:text-gray-400 
+                   focus:ring-2 focus:ring-inset 
+                   focus:ring-indigo-600 
+                   m:text-sm 
+                   sm:leading-6" />
+              </div>
+            </div>
+          </div>
+
+          <h2 class="text-xl font-semibold leading-7 text-gray-900">Información de envío</h2>
+          <div class="mt-10 grid grid-cols-12 gap-x-6 gap-y-8 sm:grid-cols-12">
+            <div v-for="(input, index) in inputs" :key="index" class="sm:col-span-6">
+              <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">
+                {{ input.label }}
+              </label>
+              <div class="mt-2">
+                <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="
+                    block w-full 
+                    rounded-md border-0 py-1.5
+                    text-gray-900 
+                    shadow-sm ring-1 
+                    ring-inset 
+                    ring-gray-300 
+                    placeholder:text-gray-400 
+                    focus:ring-2 focus:ring-inset 
+                    focus:ring-indigo-600 
+                    sm:text-sm s
+                    m:leading-6" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <h2 class="text-xl font-semibold leading-7 text-gray-900">Resumen del pedido</h2>
+          <div class="
+            border border-gray-900/10
+            rounded                 
+            text-secondary-600
+            bg-primary-50
+            dark:bg-secondary-900
+            dark:text-primary-50
+            text-base 
+            text-center 
+            py-6 
+            h-96             
+          ">
+          </div>
+        </div>
+
+      </div>
+
+
+
+
+
+    </div>
+
+    <div class="mt-6 flex items-center justify-end gap-x-6">
+      <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+      <button type="submit"
+        class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+    </div>
+  </form>
+</template>
+  
+<script>
+export default {
+
+  data() {
+    return {
+      inputs: [
+        { name: 'Nombre', label: 'Nombre', type: 'text' },
+        { name: 'Apellido', label: 'Apellido', type: 'text' }]
+    }
+  }
+}
+</script>
---
+++ src/pages/ProductsAvailable.vue
@@ -0,0 +1,103 @@
+<template>
+  <Main>
+    <!-- product -->
+    <div class="text-sm">
+      <h2 class="text-2xl font-bold tracking-tight text-gray-900 ">
+        Productos disponible
+      </h2>
+      <!-- <button @click="changeDarkMode">
+          noche
+        </button> -->
+      <div class="mt-6 grid grid-cols-1 gap-x-10 gap-y-20 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 prim">
+        <ProductCard v-for="(product, index) in productsWithDicount" :key="index" :product="product"
+          :discountGroups="discountGroups" :getProductDiscount="getProductDiscount(product)" class="
+            dark:bg-secondary-900 bg-gray-50 
+            px-2
+            py-3 
+            rounded-xl" />
+      </div>
+    </div>
+
+  </Main>
+</template>
+
+<script>
+
+import ProductCard from "../components/ProductCard.vue";
+// pinia store
+import { mapState } from 'pinia'
+import { useDefinitionsStore } from "../store/definitions.js"
+import { useSearchEngineStore } from "../store/searchEngine.js"
+
+
+export default {
+  components: {
+    ProductCard,
+  },
+
+  data() {
+    return {
+      showCart: false,
+      showDiscount: false,
+      showCurrentiIndex: null,
+    }
+  },
+
+  methods: {
+
+    getDiscountProductObject(product) {
+      return this.discounts.find(discount => discount.brand == product.brand)?.products
+        ?.find(productDiscount => productDiscount.name == product.name)
+    },
+
+    getProductDiscount(product) {
+      var discount = 0
+      var discountProductObject = this.getDiscountProductObject(product)
+
+      if (discountProductObject) {
+        if (discountProductObject?.type === 'flat') {
+          discount = discountProductObject.value
+        }
+        else {
+          discount = product.price * (discountProductObject.value / 100)
+        }
+      }
+      return discount
+    },
+
+    getDiscountedPrice(product) {
+      return product.price - this.getProductDiscount(product)
+    },
+
+    getDiscountRepresentation(product) {
+      var textDiscount = '-'
+      var discountObject = this.getDiscountProductObject(product)
+      var value = discountObject?.value
+
+      if (discountObject?.type === 'flat') {
+        textDiscount += `$${value}`
+      }
+      else {
+        textDiscount += `${value}%`
+      }
+      return textDiscount
+    },
+  },
+
+  computed: {
+
+    ...mapState(useSearchEngineStore, ['search']),
+    ...mapState(useDefinitionsStore, ['discounts', 'discountGroups', 'products']),
+
+    productsWithDicount() {
+      return this.products.filter(product => product.name.toLowerCase().includes(this.search.toLowerCase())).map(product => {
+        product.discountedPrice = this.getDiscountedPrice(product)
+        product.productDiscount = this.getProductDiscount(product)
+        product.discountRepresentation = this.getDiscountRepresentation(product)
+        return product
+      })
+    },
+
+  }
+}
+</script>
