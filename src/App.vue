c6bd0be2cb90cde31aa35e22277571c4f34cb839
emiles23 <emilesalbornoz@gmail.com>
2024-07-25 22:48:38 -0400

    feat: dynamic components

--- src/App.vue
+++ src/App.vue
@@ -1,126 +1,46 @@
 <template>
   <Main>
-    <!-- product -->
-    <div class="text-sm">
-      <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 class="text-2xl font-bold tracking-tight text-gray-900 ">
-          Productos disponible
-        </h2>
-        <!-- <button @click="changeDarkMode">
-          noche
-        </button> -->
-        <div class="mt-6 grid grid-cols-1 gap-x-10 gap-y-20 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 prim">
-          <ProductCard v-for="(product, index) in productsWithDicount" :key="index" :product="product"
-            :discountGroups="discountGroups" :getProductDiscount="getProductDiscount(product)" class="
-            dark:bg-secondary-900 bg-gray-50 
-            px-2
-            py-3 
-            rounded-xl" />
-        </div>
+    <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+
+      <div class="mb-5">
+        <nav aria-label="Tabs" class="ec lx aci acn aea bbo text-primary-100">
+          <button v-for="(tab, index) in tabs" :key="index" @click="currentTab = tab.component">
+            <span class="
+              bg-secondary-1000 p-3 
+              rounded-s-lg border-r-2 
+              border-primary-100">
+              {{ tab.name }}
+            </span>
+          </button>
+        </nav>
       </div>
+      <!-- product -->
+       <component :is="currentTab"></component>
     </div>
   </Main>
 </template>
-
 <script>
 
-import ProductCard from "./components/ProductCard.vue";
 import Main from "./layout/Main.vue";
-// import { shoppingCartStore } from "./store.js";
-
-// import { definitions } from "./store.js";
-
-// pinia store
-import { mapState } from 'pinia'
-import { useSearchEngineStore } from "./store/searchEngine.js"
-import { useDefinitionsStore } from "./store/definitions.js"
-// import { useShoppingCartStoreStore } from "./store/shoppingCartStore.js"
+import ProductsAvailable from "./pages/ProductsAvailable.vue";
+import Checkout from "./pages/Checkout.vue";
 
 
 export default {
-  components: {
-    ProductCard,
-    Main,
-  },
 
   data() {
     return {
-      showCart: false,
-      showDiscount: false,
-      showCurrentiIndex: null,
-      // shoppingCartStore,
-      // definitions,
-
+      currentTab:'ProductsAvailable',
+      tabs: [
+        { name: 'Productos', component: 'ProductsAvailable' },
+        { name: 'Checkout', component: 'Checkout' }
+      ],
     }
   },
-  updated() {
-
-    // console.log(this.getDiscountGroupAmount())
-    // console.log(this.brandAdded(discount))
+  components: {
+    ProductsAvailable,
+    Checkout,
+    Main,
   },
-
-  methods: {
-
-    // changeDarkMode() {
-    //   document.documentElement.classList.toggle("dark")
-    // },
-
-    getDiscountProductObject(product) {
-      return this.discounts.find(discount => discount.brand == product.brand)?.products
-        ?.find(productDiscount => productDiscount.name == product.name)
-    },
-    getProductDiscount(product) {
-
-      var discount = 0
-      var discountProductObject = this.getDiscountProductObject(product)
-
-      if (discountProductObject) {
-        if (discountProductObject?.type === 'flat') {
-          discount = discountProductObject.value
-        }
-        else {
-          discount = product.price * (discountProductObject.value / 100)
-        }
-      }
-
-      return discount
-    },
-
-    getDiscountedPrice(product) {
-      return product.price - this.getProductDiscount(product)
-    },
-
-    getDiscountRepresentation(product) {
-      var textDiscount = '-'
-      var discountObject = this.getDiscountProductObject(product)
-      var value = discountObject?.value
-
-      if (discountObject?.type === 'flat') {
-        textDiscount += `$${value}`
-      }
-      else {
-        textDiscount += `${value}%`
-      }
-      return textDiscount
-    },
-  },
-
-  computed: {
-    
-    ...mapState(useSearchEngineStore, ['search']),
-    ...mapState(useDefinitionsStore, ['discounts', 'discountGroups', 'products']),
-    // ...mapState(useShoppingCartStoreStore, ['products']),
-
-    productsWithDicount() {
-      return this.products.filter(product => product.name.toLowerCase().includes(this.search.toLowerCase())).map(product => {
-        product.discountedPrice = this.getDiscountedPrice(product)
-        product.productDiscount = this.getProductDiscount(product)
-        product.discountRepresentation = this.getDiscountRepresentation(product)
-        return product
-      })
-    },
-
-  }
-
 }
 </script>
