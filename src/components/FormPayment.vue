732746e598da1fad72e4db0a97291ba1826e85d1
emiles23 <emilesalbornoz@gmail.com>
2024-07-29 23:52:02 -0400

    refactor: modify the arrangement rows

--- src/components/FormPayment.vue
+++ src/components/FormPayment.vue
@@ -1,30 +1,31 @@
 <template>
-    <form class=" sm:text-sm">
-        <div v-for="(row, index) in rows" :key="index" class="grid grid-cols-12 gap-4 ">
-            <div v-for="(field, index) in row.fields" :class="row.class">
-                <div v-if="field.type !== 'radio'">
-                    <label for="first-name" class="block text-sm font-medium leading-6 text-gray-700 dark:text-gray-300">
+    <form class=" sm:text-sm block text-sm font-medium leading-6 text-gray-700 dark:text-gray-300">
+        <H1Title>Método Pago </H1Title>
+        <div v-for="(row, index) in rows" :key="index" class="grid grid-cols-12 gap-4 pt-5 ">
+            <div v-for="(field, index) in row.fields" :class="row.class" >
+                <div v-if="field.type === 'text'">
+                    <label for="first-name" class="">
                         {{ field.label }}
                     </label>
                     <fieldsForm v-model="field.value" :type="field.type" />
                 </div>
-                <div v-else>
-                    <input v-model="field.value" :type="field.type" />
-                    <label>{{ field.label }}</label>
+                <div v-else-if="field.type === 'radio'" v-for="(option, index) in field.options" class=" flex ">
+                    <input v-model="field.value" :type="field.type" :value="option.value" />
+                    <label class="pl-2">{{ option.label }}</label>
                 </div>
             </div>
         </div>
     </form>
 </template>
-
-
 <script>
 import FieldsForm from "../components/fieldsForm.vue";
+import H1Title from "../components/H1Title.vue";
 
 export default {
 
     components: {
         FieldsForm,
+        H1Title
 
     },
 
@@ -32,11 +33,19 @@ export default {
         return {
             rows: [
                 {
-                    class: 'col-span-4 my-4',
+                    class: 'col-span-12 flex w-full gap-12',
                     fields: [
-                        { name: 'payment-type', label: 'Tarjeta de crédito', type: 'radio', value: '' },
-                        { name: 'payment-type', label: 'PayPal', type: 'radio', value: '' },
-                        { name: 'payment-type', label: 'eTransfer', type: 'radio', value: '' },
+                        {
+
+                            label: 'Tipo de Pago',
+                            type: 'radio',
+                            value: 0,
+                            options: [
+                                { label: 'Tarjeta de crédito', value: 0 },
+                                { label: 'PayPal', value: 1 },
+                                { label: 'eTransfer', value: 2 },
+                            ]
+                        }
                     ]
                 },
 
